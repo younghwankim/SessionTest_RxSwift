847d83ffa4be0e7bcd16b72db40bd0722eccb5d5
Younghwan Kim <younghwan.kim@cibc.com>
2018-11-19 11:09:09 -0500

    Initial Commit

---
+++ SessionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SessionTest
+//
+//  Created by Younghwan Kim on 2018-11-19.
+//  Copyright © 2018 Younghwan Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
